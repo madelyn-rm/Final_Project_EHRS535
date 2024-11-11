8bbc71093a6250356e4fa6fa3b4342457a6eaea5
Laura Moore <lmoore@Lauras-MacBook-Pro-2.local>
2024-11-11 11:11:11 -0700

    Notes for figures

--- Final_Project_MM.R
+++ Final_Project_MM.R
@@ -3,7 +3,11 @@ library(ggthemes)
 library(readr)
 most_visited_nps_species_data <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2024/2024-10-08/most_visited_nps_species_data.csv')
 
-#hi
-#hello
-#hi
-#helloagain
+# 1. Map of invasive animals  
+
+
+# 2. Heat map animals vs national parks 
+
+
+# 3. Graph that correlates how many threatened or endaged species there are and then
+#how many non-native species there are
