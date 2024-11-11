e377b7b4f1c329171ecbd856a0a0b9ef612786b5
Laura Moore <lmoore@Lauras-MacBook-Pro-2.local>
2024-11-11 10:47:27 -0700

    added library

--- Final_Project_MM.R
+++ Final_Project_MM.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(ggthemes)
+library(readr)
 most_visited_nps_species_data <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2024/2024-10-08/most_visited_nps_species_data.csv')
 
 
