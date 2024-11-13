1b9fac7a887cc930965ab84dce7f25eb5436bebd
Laura Moore <lmoore@Lauras-MacBook-Pro-2.local>
2024-11-13 10:41:23 -0700

    Creating a new dataframe that can be used for the heatmap that filters for non-native species and summarizes the observations by parks

--- Final_Project_MM.R
+++ Final_Project_MM.R
@@ -1,12 +1,20 @@
 library(tidyverse)
 library(ggthemes)
 library(readr)
-most_visited_nps_species_data <- read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2024/2024-10-08/most_visited_nps_species_data.csv')
+library(dplyr)
+
+most_visited_nps_species_data <- read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2024/2024-10-08/most_visited_nps_species_data.csv")
+
+
 
 # 1. Map of invasive animals  
 
 
-# 2. Heat map animals vs national parks 
+# 2. Heat map invasive species vs national parks 
+#x-axis will be national parks, y-axis will be kingdom (category name)
+
+invasive_data <- most_visited_nps_species_data %>% 
+  select(ParkName, CategoryName, Nativeness, Observations)
 
 
 # 3. Graph that correlates how many threatened or endaged species there are and then
