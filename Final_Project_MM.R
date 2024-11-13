c0ea89a07b39e9f7cc4326d047827c2e35b03d61
Madelyn MacLaughlin <madelyn.rmac@gmail.com>
2024-11-13 10:42:36 -0700

    Began filtering and graphing - MM

--- Final_Project_MM.R
+++ Final_Project_MM.R
@@ -10,6 +10,22 @@ most_visited_nps_species_data <- read_csv('https://raw.githubusercontent.com/rfo
 
 
 # 3. Graph that correlates how many threatened or endangered species there are and then
-#how many non-native species there are
+# how many non-native species there are. Size of points is size of national park,
+# also include how many visitors there are per area of national park (color)
+
+nps_species <- most_visited_nps_species_data
 library(plotly)
+
+# endangered, threatened, extinct, 
+# proposed similarity of appearance (threatened), 
+# Experimental, nonessential populations of endangered species
+et_nn_species <- nps_species %>% 
+  filter(TEStatus %in% c("E", "T", "D3A", "PSAT", "E, EXPN") |
+           Nativeness == "Non-native")
+
+ggplot(et_nn_species, aes(x = ))
+
+
+
+
 
